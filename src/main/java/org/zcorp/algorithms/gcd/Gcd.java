51b203bed21c04540f02807f16bfe4b3faec7e8c
weofferservice <weofferservice@hotmail.com>
2019-01-16 19:58:28 +0300

    Добавил javadoc к интерфейсу

--- src/main/java/org/zcorp/algorithms/gcd/Gcd.java
+++ src/main/java/org/zcorp/algorithms/gcd/Gcd.java
@@ -1,5 +1,12 @@
 package org.zcorp.algorithms.gcd;
 
 public interface Gcd {
+    /**
+     * Наибольший общий делитель (НОД)
+     *
+     * @param m неотрицательное целое число
+     * @param n неотрицательное целое число
+     * @return наибольший общий делитель чисел m и n
+     */
     double gcd(int m, int n);
 }
